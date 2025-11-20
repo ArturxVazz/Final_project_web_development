fa23b6a03dce980af67fd616ba9357034931de3e
Artur Silva Vaz <162496351+ArturxVazz@users.noreply.github.com>
2025-11-20 03:03:11 -0300

    chore: define package structure (entity,repository, service, controller)

---
+++ GerenciadorDeTarefas/src/test/java/Tarefas/GerenciadorDeTarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package Tarefas.GerenciadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
